418bab6349b944b0b2084c5d106b49305af05ea0
Charles Bezerra <charles-bezerra>
2016-10-17 15:13:19 -0300

    alterações 1

--- Teste_de_sensores.ino.ino
+++ Teste_de_sensores.ino.ino
@@ -11,15 +11,13 @@ int pino_Temp_Humi = A1;
 int pino_UV = A2;
 int pino_Qualidade_Ar = A3;
 
-// instacias
+// instancias
 AirQuality airqualitysensor;
-
 Barometer myBarometer;
 
 
-
+////////////////////////////////////////////////////////////////////////////////////////
 class Barometro : public Barometer{
-
 public:
         void ilustrar();
 private:
@@ -27,9 +25,11 @@ private:
         double pressure;
 };
 
+
 void Barometro::ilustrar(){
             temperature = myBarometer.bmp085GetTemperature(myBarometer.bmp085ReadUT());
             pressure = myBarometer.bmp085GetPressure(myBarometer.bmp085ReadUP());
+            Serial.println("==========|Sensor Barometro|=========");
             Serial.print("Temperature: ");
             Serial.print(temperature, 2); //display 2 decimal places
             Serial.println("deg C");
@@ -37,8 +37,10 @@ void Barometro::ilustrar(){
             Serial.print("Pressure: ");
             Serial.print(pressure, 0); //whole number only.
             Serial.println(" Pa");
-            Serial.println();  }
-
+            Serial.println(); 
+  
+            }
+//////////////////////////////////////////////////////////////////////////////////////
 class UV{
 public:
       void uv(int pino);
@@ -55,13 +57,14 @@ void UV::uv(int pino){
             delay(2);
         }
         float meanVal = aux/1024;
-        Serial.print("The current UV index is:");
-        Serial.print((meanVal*1000/4.3-83)/21);
-        Serial.print("\n");
+        Serial.println("============|Sensor UV|============");
+        Serial.print("O Indice de UV atual e: ");
+        Serial.println((meanVal*1000/4.3-83)/21);
+        Serial.println();
         delay(20);
 }
 
-
+/////////////////////////////////////////////////////////////////
 class Qualidade_Ar : public AirQuality{
 public:
       void ar();
@@ -71,19 +74,40 @@ private:
 };
 
 void Qualidade_Ar::ar(){
+      Serial.println("=======|Sensor Qualidade do Ar|=======");
       current_quality = airqualitysensor.slope();
       if (current_quality >= 0){
           if (current_quality==0)
-                Serial.println("High pollution! Force signal active");
-          else if (current_quality==1)
-                Serial.println("High pollution!");
-          else if (current_quality==2)
-                Serial.println("Low pollution!");
-          else if (current_quality ==3)
-                Serial.println("Fresh air");  }
-  }
+                Serial.println("Alta poluição! Force signal active");
+          else if (current_quality == 1)
+                Serial.println("Alta paluição!");
+          else if (current_quality == 2)
+                Serial.println("Baixa poluição!");
+          else if (current_quality == 3)
+                Serial.println("Ar fresco");  }
+}
+
+///////////////////////////////////////////////////////////////////
 
 
+class LUZ{
+  //OPS:  Não sei a unidade de medida de luz ou como representar!
+  // Não encontrei o datasheet
+};
+
+
+///////////////////////////////////////////////////////////////
+class TEMP_HUMI{
+};
+///////////////////////////////////////////////////////////////
+
+
+//instancias
+Qualidade_Ar myQualidade_Ar;
+Barometro myBarometro;
+UV myUV;
+
+///////////////////////////////////////////////////////////////
 
 
 
@@ -101,23 +125,26 @@ void setup()
 
 
 void loop(){
-          
+          delay(1000);
+          Serial.println("");
+          Serial.println("");
+          Serial.println("");
+          Serial.println("");
+          Serial.println("");
+          Serial.println("");
+          myBarometro.ilustrar();
+          myUV.uv(pino_UV);
+          myQualidade_Ar.ar();
 }
-ISR(TIMER1_OVF_vect)
-{
-	if(airqualitysensor.counter==61)//set 2 seconds as a detected duty
-	{
+ISR(TIMER1_OVF_vect){
+if(airqualitysensor.counter==22)/*set 2 seconds as a detected duty*/
+        {
 
-			airqualitysensor.last_vol=airqualitysensor.first_vol;
-			airqualitysensor.first_vol=analogRead(pino_Qualidade_Ar);
-			airqualitysensor.counter=0;
-			airqualitysensor.timer_index=1;
-			PORTB=PORTB^0x20;
-	}
-	else
-	{
-		airqualitysensor.counter++;
-	}
-
-
+         airqualitysensor.last_vol=airqualitysensor.first_vol;
+         airqualitysensor.first_vol=analogRead(pino_Qualidade_Ar);
+         airqualitysensor.counter=0;
+         airqualitysensor.timer_index=1;
+         PORTB=PORTB^0x20;  
+         }
+         else{ airqualitysensor.counter++; }
 }
